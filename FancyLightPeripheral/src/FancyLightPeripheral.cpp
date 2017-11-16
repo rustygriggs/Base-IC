24c1de42285fca6480310e17ffc20510fdef539d
Brennon Loveless <brennon.loveless@gmail.com>
2017-11-15 20:59:48 -0700

    Added the first working FancyLEDPeripheral.

---
+++ FancyLightPeripheral/src/FancyLightPeripheral.cpp
@@ -0,0 +1,217 @@
+#include <Arduino.h>
+#include <BaseIC.h>
+#include <SoftwareSerial.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+// SoftwareSerial sSerial(8, 9); // RX, TX
+
+// #define VERBOSE false
+
+// BaseIC baseIC = BaseIC(sSerial, VERBOSE);
+
+/**
+ * Register a hex, range, toggle, and read service.
+ * Everthing needs to be characters. The zigbee module encoded and decodes
+ * characters better than it does integers.
+ *
+uint8_t inputServices[1] = {'3'}; // Define one Toggle Input
+uint8_t outputServices[1] = {'3'}; // Define one Toggle Output
+uint8_t name[10] = {'L', 'E', 'D', ' ', 'L', 'I', 'G', 'H', 'T', '2'};
+
+void responseListener(ZBRxResponse &rx, uintptr_t) {
+    uint8_t *data = rx.getFrameData() + rx.getDataOffset();
+    char buffer[500];
+    int dataPos = 0, bufferPos = 0, serviceId = 0, serviceNumber = 0;
+
+    // Get Service ID by parsing the frame data until the first tab.
+    for (dataPos = 0; ((dataPos < 500) && (dataPos < rx.getDataLength())); dataPos++) {
+        if (data[dataPos] == '\t') {
+            break;
+        }
+
+        buffer[bufferPos++] = data[dataPos];
+    }
+
+    // Then the data that is stored in the buffer can be converted to an int to represent the Service Id.
+    dataPos++;
+    buffer[bufferPos++] = '\0';
+    serviceId = atoi(buffer);
+
+    // Get Service Number by parsing the frame data until the first tab.
+    bufferPos = 0;
+    for (; ((dataPos < 500) && (dataPos < rx.getDataLength())); dataPos++) {
+        if (data[dataPos] == '\t') {
+            break;
+        }
+
+        buffer[bufferPos++] = data[dataPos];
+    }
+
+    // Then the data that is stored in the buffer can be converted to an int to represent the Service Number.
+    dataPos++;
+    buffer[bufferPos++] = '\0';
+    serviceNumber = atoi(buffer);
+
+    // Get Value by parsing the frame data until the new line.
+    bufferPos = 0;
+    for (; ((dataPos < 500) && (dataPos < rx.getDataLength())); dataPos++) {
+        if (data[dataPos] == '\n') {
+            break;
+        }
+
+        buffer[bufferPos++] = data[dataPos];
+    }
+
+    // We don't have to do anything with the buffer now. It will contain a char representation of the value.
+    buffer[bufferPos++] = '\0';
+
+    if (VERBOSE) {
+        Serial.print("Service ID:\t");
+        Serial.println(serviceId);
+        Serial.print("Service Number:\t");
+        Serial.println(serviceNumber);
+        Serial.print("Value:\t\t");
+        Serial.println(buffer);
+    }
+
+    // If the response is for the toggle service and is service number 1.
+    // THEN TOGGLE!!!
+    if (serviceId == 3 && serviceNumber == 1) {
+        if (digitalRead(13) == HIGH) {
+            digitalWrite(13, LOW);
+        } else {
+            digitalWrite(13, HIGH);
+        }
+    }
+}
+*/
+
+#define PIN 3
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+    pinMode(13, OUTPUT);
+    Serial.begin(9600);
+
+    // baseIC.begin();
+
+    // baseIC.setNetworkID();
+
+    // baseIC.registerModule(
+    // name, sizeof(name),
+    // inputServices, sizeof(inputServices),
+    // outputServices, sizeof(outputServices));
+
+    // baseIC.attachListener(responseListener);
+
+
+    strip.begin();
+    strip.show(); // Initialize all pixels to 'off'
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+    for (uint16_t i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, c);
+        strip.show();
+        delay(wait);
+    }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+    WheelPos = 255 - WheelPos;
+    if (WheelPos < 85) {
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    }
+    if (WheelPos < 170) {
+        WheelPos -= 85;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    }
+    WheelPos -= 170;
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+void rainbow(uint8_t wait) {
+    uint16_t i, j;
+
+    for (j = 0; j < 256; j++) {
+        for (i = 0; i < strip.numPixels(); i++) {
+            strip.setPixelColor(i, Wheel((i + j) & 255));
+        }
+        strip.show();
+        delay(wait);
+    }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+    uint16_t i, j;
+
+    for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
+        for (i = 0; i < strip.numPixels(); i++) {
+            strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+        }
+        strip.show();
+        delay(wait);
+    }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+    for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
+        for (int q = 0; q < 3; q++) {
+            for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+                strip.setPixelColor(i + q, c); //turn every third pixel on
+            }
+            strip.show();
+
+            delay(wait);
+
+            for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+                strip.setPixelColor(i + q, 0); //turn every third pixel off
+            }
+        }
+    }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+    for (int j = 0; j < 256; j++) { // cycle all 256 colors in the wheel
+        for (int q = 0; q < 3; q++) {
+            for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+                strip.setPixelColor(i + q, Wheel((i + j) % 255)); //turn every third pixel on
+            }
+            strip.show();
+
+            delay(wait);
+
+            for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+                strip.setPixelColor(i + q, 0); //turn every third pixel off
+            }
+        }
+    }
+}
+
+void loop() {
+    // Continuously let xbee read packets and call callbacks.
+    // baseIC.loop();
+
+    // Some example procedures showing how to display to the pixels:
+    colorWipe(strip.Color(255, 0, 0), 50); // Red
+    colorWipe(strip.Color(0, 255, 0), 50); // Green
+    colorWipe(strip.Color(0, 0, 255), 50); // Blue
+    //colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
+    // Send a theater pixel chase in...
+    theaterChase(strip.Color(127, 127, 127), 50); // White
+    theaterChase(strip.Color(127, 0, 0), 50); // Red
+    theaterChase(strip.Color(0, 0, 127), 50); // Blue
+
+    rainbow(20);
+    rainbowCycle(20);
+    theaterChaseRainbow(50);
+}
